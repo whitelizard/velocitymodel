f0dc0606c3e86dd06008ebf10bd52831238d78ba
Esbjörn Blomquist <esbblo@gmail.com>
2016-08-02 17:39:40 +0200

    Added support for circular interval

---
+++ src/test/velocitymodel.js
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+import VelocityModel from '../velocitymodel';
+import { Range } from 'immutable';
+
+const vm = new VelocityModel(1, 1, 1, { min: -10, max: 190, circular: true });
+vm.setTarget(180);
+expect(vm.circular).to.be.true;
+expect(vm.PID.target).to.equal(-20);
+vm.pos = 185;
+vm.setTarget(-5);
+expect(vm.PID.target).to.equal(195);
+Range(1, 5).forEach(() => vm.update()); // update a couple of times to control pos toward target
+expect(Math.sign(vm.pos)).to.equal(-1); // same side on the interval as target => negative sign
--- src/velocitymodel.js
+++ src/velocitymodel.js
@@ -2,14 +2,17 @@ import PIDController from 'node-pid-controller';
 
 export default class VelocityModel {
 
-  constructor(P, D, dt=1, args) {
-    this.min = (args && args.minStopAngle) || 2;
-    this.max = (args && args.maxStopAngle) || 358;
+  constructor(P, D, dt = 1, args) {
+    this.min = (args && args.min) || 0;
+    this.max = (args && args.max) || 100;
+    this.interval = this.max - this.min;
+    this.halfInterval = this.interval / 2;
+    this.circular = (args && args.circular) || false;
     // this.maxVelocity = (args && args.maxVelocity) || 0.5; // TODO: ??
     this.dt = dt;
-    this.dt2 = 0.5 * dt * dt;
+    // this.dt2 = 0.5 * dt * dt;
     this.v = 0;
-    this.pos = 0; //(args && args.start) ||
+    this.pos = 0; // (args && args.start) ||
     this.PID = new PIDController(P, 0, D, dt);
   }
 
@@ -18,17 +21,45 @@ export default class VelocityModel {
     const force = this.PID.update(this.pos);
     this.v += force * this.dt;
     this.pos += this.v * this.dt;// + this.dt2*force;
-    if (this.pos < this.min) {
-      this.v = 0;
-      this.pos = this.min;
-    }
-    if (this.pos > this.max) {
-      this.v = 0;
-      this.pos = this.max;
+    if (this.circular) {
+      if (!this.inside(this.PID.target)) {
+        // if we are in the state of a target outside the interval,
+        // check if this.pos also is outside now => correct both
+        if (this.pos < this.min) {
+          this.PID.setTarget(this.PID.target + this.interval);
+          this.pos += this.interval;
+        } else if (this.pos > this.max) {
+          this.PID.setTarget(this.PID.target - this.interval);
+          this.pos -= this.interval;
+        }
+      }
+    } else {
+      if (this.pos < this.min) {
+        this.v = 0;
+        this.pos = this.min;
+      }
+      if (this.pos > this.max) {
+        this.v = 0;
+        this.pos = this.max;
+      }
     }
   }
 
-  setTarget(pos) {
-    this.PID.setTarget(pos);
+  inside(pos) {
+    return pos < this.max && pos > this.min;
+  }
+
+  setTarget(target) {
+    if (this.circular) {
+      const error = target - this.pos;
+      const absError = Math.abs(error);
+      if (absError < this.interval - absError) {
+        this.PID.setTarget(this.pos + error);
+      } else {
+        this.PID.setTarget(this.pos + Math.sign(-error) * (this.interval - absError));
+      }
+    } else {
+      this.PID.setTarget(target);
+    }
   }
 }
